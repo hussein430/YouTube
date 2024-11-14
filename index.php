73cff14105a59e0a33bd551b9d53ddf1a6271be4
hussein430 <hussen.alaa1222@gmail.com>
2024-11-14 11:29:33 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YouTube</title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- Css File -->
+    <link rel="stylesheet" href="css/main.css" />
+</head>
+
+<body>
+    <div id="pageContainer">
+        <div id="headerContainer">
+            <button class="navShowHide">Click Me!</button>
+        </div>
+
+        <div id="sideNavContainer"></div>
+
+        <div id="mainSectionContainer">
+            <div id="mainContentContainer">
+                Hello World
+            </div>
+        </div>
+    </div>
+
+    <!-- jQuery CDN -->
+    <script src="https://code.jquery.com/jquery-3.7.1.js" crossorigin="anonymous"></script>
+
+    <!-- Bootstrap Bundle.js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+    <!-- Our JavaScript -->
+    <script src="js/app.js"></script>
+</body>
+
+</html>
